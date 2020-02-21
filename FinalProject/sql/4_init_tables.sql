6775e03b7bb08e86daa00c69d2b87b77e5b92c7b
Tsarionok <tsarionok@gmail.com>
2020-02-21 18:53:03 +0300

    init table fix

--- FinalProject/sql/4_init_tables.sql
+++ FinalProject/sql/4_init_tables.sql
@@ -1,2 +1,2 @@
 USE ` movie_viewing_service_db ` ;
-INSERT INTO ` users ` (` id `, ` login `, ` password `, ` role `) VALUES (1, 'admin',    'admin', 0) ;
+INSERT INTO ` user ` (` id `, ` login `, ` password `, ` role `) VALUES (1, 'admin',    'adminpass', 0) ;
