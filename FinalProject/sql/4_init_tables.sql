7b00ef8136e3bbf98486086a01585dbeee92eb2e
Tsarionok <tsarionok@gmail.com>
2020-02-21 18:56:09 +0300

    fix sql script bugs

--- FinalProject/sql/4_init_tables.sql
+++ FinalProject/sql/4_init_tables.sql
@@ -1,2 +1,2 @@
 USE ` movie_viewing_service_db ` ;
-INSERT INTO ` user ` (` id `, ` login `, ` password `, ` role `) VALUES (1, 'admin',    'adminpass', 0) ;
+INSERT INTO ` user ` (` id `, ` login `, ` password `, ` role `) VALUES (1, 'admin', 'adminpass', 0) ;
